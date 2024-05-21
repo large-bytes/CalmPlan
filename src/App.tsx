f5ab3f2d0df1b3f7f9f1564477cf5a5eeb410b53
josephclander <josephclander@gmail.com>
2024-05-21 16:19:31 +0100

    Remove details on tasklist

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 // file: src/App.tsx
-import Header from "./elements/Header";
 import DisplayList from "./components/display_list/DisplayList";
 
 function App() {
   return (
     <>
-      <Header>Home</Header>
       <DisplayList />
     </>
   );
--- src/routes/Tasks.tsx
+++ src/routes/Tasks.tsx
@@ -2,7 +2,6 @@
 import { useTasks } from "../contexts/TasksContext";
 
 import TaskList from "../components/task_list/TaskList";
-import Header from "../elements/Header";
 import TaskForm from "../components/task_form/TaskForm";
 
 const Tasks: React.FC = () => {
@@ -10,7 +9,6 @@ const Tasks: React.FC = () => {
 
   return (
     <>
-      <Header>Tasks</Header>
       <TaskList tasks={tasks} />
       <TaskForm />
     </>
